98c3332238dd3b52cb8e6cf53fb067d28bc98219
Maxwell-Yang-2001 <migacanada@gmail.com>
2020-12-27 11:32:17 -0800

    update server to pug and set up connection

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
+const { RSA_NO_PADDING } = require('constants');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+  res.send('Working properly');
 });
 
 module.exports = router;
