f5e56a7063b7c46dd3cd951d537d20d589ad2514
Maxwell-Yang-2001 <migacanada@gmail.com>
2021-01-02 02:44:59 -0800

    Implement sign in / up page UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import HomeScreen from './homeScreen/HomeScreen';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/">
-          <HomeScreen />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  const [userId, setUserId] = useState(undefined);
+
+  if (userId === undefined) {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <HomeScreen />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  } else 
+  {
+    return null;
+  }
 }
 
 export default App;
